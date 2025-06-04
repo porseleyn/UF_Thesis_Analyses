9bad683d7da9ccebddf8db239d5923d75646e809
Emma Boom <emma.boom2006@gmail.com>
2025-06-04 10:24:20 +0200

    Added all the data

---
+++ extract_data.py
@@ -0,0 +1,11 @@
+class ExtractData:
+    def ___init___(self):
+        """Initialise the data extraction."""
+        
+    def getSubs(self):
+        """Explanation."""
+        return 
+        
+    def earlyWin(self):
+        """Explanation."""
+        
